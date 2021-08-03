9736df8866cf1fd8ac934f82992b0c9f124fa2be
theCromers <kayla.e.cromer@gmail.com>
2021-08-02 20:46:26 -0400

    initial commit of square

---
+++ square.java
@@ -0,0 +1,17 @@
+package lab4_1302;
+
+public class square {
+	private int x, y, sideLength;
+	
+	public square(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.sideLength = 5;
+	}
+	public square(int sideLength) {
+		this.x = 0;
+		this.y = 0;
+		this.sideLength = 5;
+	}
+
+}
---
+++ squareClient.java
@@ -0,0 +1,10 @@
+package lab4_1302;
+
+public class squareClient {
+	
+	public static void main(String args[]) {
+		square squ1 = new square(1,2);
+		square squ2 = new square(5);
+	}
+
+}
